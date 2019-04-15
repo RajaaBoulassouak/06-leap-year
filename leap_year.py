2aee5c734fcc023012dcd5776269644e0cf43f0b
Rajaa Boulassouak <39357526+RajaaBoulassouak@users.noreply.github.com>
2019-04-15 08:39:59 -0600

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -5,4 +5,7 @@
 # Don't forget to reach out for help after your own due diligence
 
 def is_leap_year(year):
-    return ''
+    if year / 4 == 0 or year / 400 == 0:
+        return 'true'
+    else:
+        return 'false'
